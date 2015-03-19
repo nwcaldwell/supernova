5a1b5af322117786eb2fd2fcb288c050ff312a91
nwcaldwell <nathanwcaldwell@gmail.com>
2015-03-19 12:01:11 -0400

    Added resource files and modified homescreen

--- app/src/main/java/com/nathanwcaldwell/supernova/MainActivity.java
+++ app/src/main/java/com/nathanwcaldwell/supernova/MainActivity.java
@@ -1,21 +1,48 @@
 package com.nathanwcaldwell.supernova;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.grandfell);
+        mp.setLooping(true);
+        mp.start();
     }
 
+    @Override
+    protected void onPause() {
+        mp.stop();
+        mp.release();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mp = MediaPlayer.create(this, R.raw.grandfell);
+        mp.setLooping(true);
+        mp.start();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,5 +74,49 @@ public class MainActivity extends ActionBarActivity {
     public void goToStoreActivity(View v) {
         Intent intent = new Intent(this, StoreActivity.class);
         startActivity(intent);
+    }
+
+    public void goToMercuryActivity(View v) {
+        Intent intent = new Intent(this, MercuryActivity.class);
+        startActivity(intent);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
     }
 }
